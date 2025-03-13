b328be4f6f3ee9c8c100b5993cb5abcb7683bb92
George Cerqueira <31051304+geocer@users.noreply.github.com>
2025-03-13 10:46:23 -0300

    Update validate.py

--- validate.py
+++ validate.py
@@ -37,3 +37,46 @@ if verificar_instancias_iniciadas(instance_ids):
     print("Todas as instâncias foram iniciadas com sucesso!")
 else:
     print("Nem todas as instâncias foram iniciadas.")
+    
+    import boto3
+
+def validate_ec2_stopped(ec2_stopped):
+    notify = []
+    ec2 = boto3.client('ec2')
+
+    for instance_ids in ec2_stopped:
+        response = ec2.describe_instances(InstanceIds=[instance_ids])
+
+        for reservation in response['Reservations']:
+            for instance in reservation['Instances']:
+                instance_id = instance['InstanceId']
+                status_checks = ec2.describe_instance_status(InstanceIds=[instance_id])
+
+                if status_checks['InstanceStatuses']:  # Verifica se há status checks
+                    instance_status = status_checks['InstanceStatuses'][0]
+                    system_status = instance_status['SystemStatus']['Status']
+                    instance_status_check = instance_status['InstanceStatus']['Status']
+
+                    print(f"Instance {instance_id}: System Status - {system_status}, Instance Status - {instance_status_check}")
+
+                    if system_status != 'ok' or instance_status_check != 'ok':
+                        print(f"Instance {instance_id} has status checks failing.")
+                        notify.append(f" {instance_ids} : System Status - {system_status}, Instance Status - {instance_status_check} ")
+                        continue
+                else:
+                    print(f"Instance {instance_id} has no status checks.")
+                    notify.append(f" {instance_ids} : No status checks available. ")
+                    continue
+
+    if len(notify) > 0:
+        topico_arn = "arn:aws:sns:sa-east-1:650501285453:cloud-latam-topic"
+        assunto = "WARN|EC2AutoStopLowers|fdaws-brazil-se-dc1homolog-prod"
+        c_mensagem = "".join(map(str, notify))
+        mensagem = f"Some instance(s) has status checks failing: {c_mensagem}"
+        sent_email_sns(topico_arn, assunto, mensagem)
+    else:
+        topico_arn = "arn:aws:sns:sa-east-1:650501285453:cloud-latam-topic"
+        # Adicione aqui o código para enviar uma mensagem de sucesso, se necessário
+
+    
+    
